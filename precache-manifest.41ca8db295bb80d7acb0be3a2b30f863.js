f64bc65d21df1988aa3fe23bc9a2fd0d745f1e95
Tyler Benning <tbenning27@gmail.com>
2018-11-18 23:03:37 -0500

    Updates

---
+++ precache-manifest.41ca8db295bb80d7acb0be3a2b30f863.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1239f07e16a46412747c",
+    "url": "/tania-react-tutorial/static/css/main.4d3eb4fe.chunk.css"
+  },
+  {
+    "revision": "1239f07e16a46412747c",
+    "url": "/tania-react-tutorial/static/js/main.1239f07e.chunk.js"
+  },
+  {
+    "revision": "5b966dc2e69f15274453",
+    "url": "/tania-react-tutorial/static/js/1.5b966dc2.chunk.js"
+  },
+  {
+    "revision": "8c88cadbcf6a050873d1",
+    "url": "/tania-react-tutorial/static/js/runtime~main.8c88cadb.js"
+  },
+  {
+    "revision": "46cbbd9789c318102ce23224b691bc00",
+    "url": "/tania-react-tutorial/index.html"
+  }
+];
